e753f8cee09c8a5892baa44ea7b199b835ce7414
Saikikky <1175923612@qq.com>
2019-04-15 20:02:33 +0800

    输出字符串的所有组合

---
+++ 输出字符串的所有组合.java
@@ -0,0 +1,69 @@
+/**
+ * 输出字符串的所有组合
+ * abc
+ * a,b,c,ab,bc,ac,abc
+ */
+
+
+/**
+ * 递归解法 时间复杂度O(n^2)
+ */
+public class Solution {
+
+    public static void combine(char[] chars, int begin, int len, StringBuffer sb) {
+        if (len == 0) {
+            System.out.println(sb);
+            return;
+        }
+        if (begin == chars.length) return;
+        // 取当前字符
+        sb.append(chars[begin]);
+        combine(chars, begin + 1, len - 1, sb);
+        // 不取当前字符
+        sb.deleteCharAt(sb.length() - 1);
+        combine(chars, begin + 1, len, sb);
+    }
+
+    public static void main(String[] args) {
+        String str = "abc";
+        char[] chars = str.toCharArray();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 1; i <= chars.length; i++) {
+            combine(chars, 0, i, sb);
+        }
+    }
+
+
+    /**
+     * 二进制解法
+     */
+    public class Solution {
+
+        public static void combine(char[] chars) {
+            if (chars == null) return;
+            int len = chars.length;
+            boolean used[] = new boolean[len]; // 默认为false
+            char cache[] =  new char[len];
+            int result = len;
+            while (true) {
+                int index = 0;
+                while (used[index]) {
+                    used[index] = false;
+                    ++result;
+                    if (++index == len) return;
+                }
+                used[index] = true;
+                cache[--result] = chars[index];
+                System.out.println(new String(cache).substring(result) + " ");
+            }
+
+        }
+
+        public static void main(String[] args) {
+            String str = "abc";
+            char[] chars = str.toCharArray();
+            combine(chars);
+
+        }
+    }
+}
