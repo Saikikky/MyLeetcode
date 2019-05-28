fb57a76c978844fd902640bba11dfe62bd884de3
Saikikky <1175923612@qq.com>
2019-05-28 09:07:29 +0800

    更改树的层次便利

--- 从上到下输出二叉树(层次遍历).java
+++ 从上到下输出二叉树(层次遍历).java
@@ -1,47 +1,36 @@
+import java.util.ArrayList;
+import java.util.ArrayDeque;
+import java.util.Queue;
 /**
- * 判断一个树是否为另一个树的子树
+ public class TreeNode {
+ int val = 0;
+ TreeNode left = null;
+ TreeNode right = null;
+
+ public TreeNode(int val) {
+ this.val = val;
+
+ }
+
+ }
  */
-class TreeNode {
-    int val = 0;
-    TreeNode left = null;
-    TreeNode right = null;
-
-    public TreeNode(int val) {
-        this.val = val;
-
-    }
-
-}
-
 public class Solution {
-    public boolean HasSubtree(TreeNode root1, TreeNode root2) {
-        boolean result = false;
+    public ArrayList<Integer> PrintFromTopToBottom(TreeNode root) {
+        // 保存TreeNode的val队列
+        Queue<TreeNode> queue = new ArrayDeque<>();
+        // 保存二叉树的结点值
+        ArrayList<Integer> result = new ArrayList<>();
+        if (root == null) return result;
+        TreeNode treeNode = null;
 
-        if (root1 != null && root2 != null) {
-            // 若俩跟结点相同，则开始判断是否root1中含有root2
-            if (root1.val == root2.val) {
-                result = DoesTree1HaveTree2(root1, root2);
-            }
 
-            // 若根结点不相同
-            if (!result) {
-                result = HasSubtree(root1.left, root2);
-            }
-            if (!result) {
-                result = HasSubtree(root1.right, root2);
-            }
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            treeNode = queue.poll();
+            result.add(treeNode.val);
+            if (treeNode.left != null) queue.offer(treeNode.left);
+            if (treeNode.right != null) queue.offer(treeNode.right);
         }
         return result;
     }
-
-    public boolean DoesTree1HaveTree2(TreeNode root1, TreeNode root2) {
-        if (root2 == null) return true;
-
-        if (root1 == null) return false;
-
-        if (root1.val != root2.val) return false;
-
-        return DoesTree1HaveTree2(root1.left, root2.left) && DoesTree1HaveTree2(root1.right, root2.right);
-    }
-
 }
