c4c45ef9cac80bc9af1c6e013fc99a7c726f6a77
Saikikky <1175923612@qq.com>
2019-05-28 10:39:48 +0800

    单例模式

---
+++ 单例模式.java
@@ -0,0 +1,32 @@
+public class Singleton {
+
+    /** 懒汉式 效率很低
+     private static Singleton instance;
+     private Singleton(){}
+
+     public static synchronized Singleton getInstance() {
+     if (instance == null) instance = new Singleton();
+     return instance;
+     }*/
+
+    /** 饿汉式 没有加锁效率提高，但类加载就初始化浪费内存
+     private static Singleton instance = new Singleton();
+     private Singleton() {}
+     public static Singleton getInstance() {
+     return instance;
+     }*/
+
+    /** 双检锁
+     private volatile static Singleton instance;
+     private Singleton(){}
+     public static Singleton getInstance() {
+     if (instance == null) {
+     synchronized (Singleton.class) {
+     if (instance == null) {
+     instance = new Singleton();
+     }
+     }
+     }
+     return instance;
+     } */
+}
