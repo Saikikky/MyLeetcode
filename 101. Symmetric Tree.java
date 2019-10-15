211540574dae6c9e50b2c87684968b73186ab09e
Saikikky <1175923612@qq.com>
2019-10-15 11:04:16 +0800

    Symmetric Tree

---
+++ 101. Symmetric Tree.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return Symmetric(root.left, root.right);
+    }
+    
+    public boolean Symmetric(TreeNode root1, TreeNode root2) {
+        if (root1 == null && root2 == null) return true;
+        if (root1 == null || root2 == null) return false;
+        if (root1.val == root2.val) return (Symmetric(root1.left, root2.right) && Symmetric(root1.right, root2.left));
+        return false;
+    }
+}
