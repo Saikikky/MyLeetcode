50e85d3d83b87d5ce8cefec0acf14a23eeb010fc
Saikikky <1175923612@qq.com>
2019-10-22 16:37:19 +0800

    旋转数组

--- 189. Rotate Array.java
+++ 189. Rotate Array.java
@@ -1,24 +1,12 @@
 class Solution {
-    public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> hashMap = getElement(nums);
-        HashMap.Entry<Integer, Integer> temp = null;
-        for (HashMap.Entry<Integer, Integer> entry : hashMap.entrySet()) {
-            if (temp == null || temp.getValue() < entry.getValue()) {
-                temp = entry;
-            }
-        }
-        return temp.getKey();
-    }
-    
-    public HashMap<Integer, Integer> getElement(int[] nums) {
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
+    public void rotate(int[] nums, int k) {
+        int[] result = new int[nums.length];
         for (int i = 0; i < nums.length; i++) {
-            if (!hashMap.containsKey(nums[i])) {
-                hashMap.put(nums[i], 1);
-            } else {
-                hashMap.put(nums[i], hashMap.get(nums[i]) + 1);
-            }
+            int temp = (i + k) % nums.length;
+            result[temp] = nums[i];
         }
-        return hashMap;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = result[i];
+        }
     }
 }
