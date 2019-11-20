f187c0e3630036660cc4140dc5dfc607ac508f95
Saikikky <1175923612@qq.com>
2019-11-20 16:01:04 +0800

    搜索二维矩阵(二分)

---
+++ 74. Search a 2D Matrix.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0] == null || matrix[0].length == 0) return false;
+        int row = -1;
+        // 先确定在哪一行
+        for (int i = 0; i < matrix.length; i++) {
+            if (target >= matrix[i][0] && target <= matrix[i][matrix[i].length - 1]) {
+                row = i;
+                break;
+            }
+        }
+        if (row == -1) return false;
+        int left = 0;
+        int right = matrix[row].length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (matrix[row][mid] == target) {
+                return true;
+            } else if (matrix[row][mid] > target) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return false;
+    }
+}
