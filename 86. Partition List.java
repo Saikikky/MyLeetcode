a7c56282a7d48515ef9b4f811618306339349d0d
Saikikky <1175923612@qq.com>
2019-12-03 20:04:50 +0800

    分隔链表(临界值的处理方法比较蠢 可以repeated)

---
+++ 86. Partition List.java
@@ -0,0 +1,36 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode partition(ListNode head, int x) {
+        ListNode h = head;
+        ListNode l1 = new ListNode(-1);
+        ListNode h1 = l1;
+        ListNode l2 = new ListNode(-1);
+        ListNode h2 = l2;
+
+        while (h != null) {
+            if (h.val < x) {
+                h1.next = h;
+                h = h.next;
+                h1 = h1.next;
+                h1.next = null;
+
+            } else {
+                h2.next = h;
+                h = h.next;
+                h2 = h2.next;
+                h2.next = null;
+
+            }
+
+        }
+        h1.next = l2.next;
+        return l1.next;
+    }
+}
