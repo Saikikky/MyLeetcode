e729d9aa1792638e0dc1cdf116640fa0eb7299be
Saikikky <1175923612@qq.com>
2019-12-13 16:39:14 +0800

    有重复数字的子集(repeated) 添加新解法

--- 90. Subsets II.java
+++ 90. Subsets II.java
@@ -1,3 +1,51 @@
+/**
+数组 [ 1 2 2 ] 
+[ ] 的所有子串 [ ]
+[ 1 ] 的所有子串 [ ] [ 1 ] 
+[ 1 2 ] 的所有子串 [ ] [ 1 ] [ 2 ][ 1 2 ]
+[ 1 2 2 ] 的所有子串 [ ] [ 1 ] [ 2 ] [ 1 2 ] [ 2 ] [ 1 2 ] [ 2 2 ] [ 1 2 2 ] 
+记住新解的位置从新解开始增加 原先没有重复的元素的时候是直接将所有的结果增加
+**/
+class Solution {
+
+    List<List<Integer>> results;
+    // boolean[] flag;
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        Arrays.sort(nums);
+        results = new ArrayList<>();
+        // 添加空数字为其中一个结果
+        results.add(new ArrayList<>());
+        // 保存每一步的新解开始的位置
+        int start = 1;
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> result = new ArrayList<>();
+            for (int j = 0; j < results.size(); j++) {
+                List<Integer> ans = results.get(j);
+                // 若有重复数字跳过
+                if (i > 0 && nums[i] == nums[i - 1] && j < start) {
+                    continue;
+                }
+                List<Integer> temp = new ArrayList<>(ans);
+                temp.add(nums[i]);
+                result.add(temp);
+            }
+
+            // 新解的位置
+            start = results.size();
+            results.addAll(result);
+        }
+        return results;
+    }
+
+
+
+
+
+
+
+
+
+
 class Solution {
 
     List<List<Integer>> results;
